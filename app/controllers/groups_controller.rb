5d65a1378d6688574112f03272620b8251a06f15
mirai <miraiqie@gmail.com>
2018-10-23 22:31:45 +0800

    implement groups edit&update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,6 +18,16 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to root_path, notice: "Eidt Group Success."
+  end
+
   private
 
   def group_params
